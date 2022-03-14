bbcca9c805a066ce393018be3e24f46169aa496c
lenin mamani <lenin.mamani@tannua.com>
2022-03-14 13:02:25 -0500

    changed code

--- src/script.js
+++ src/script.js
@@ -3,45 +3,75 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
-// Debug
-const gui = new dat.GUI()
+let camera, scene, renderer, video
+let sphere, sizes
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+const init = () => {
+    // Debug
+    const gui = new dat.GUI()
 
-// Scene
-const scene = new THREE.Scene()
+    // Canvas
+    const canvas = document.querySelector('canvas.webgl')
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+    // Scene
+    scene = new THREE.Scene()
 
-// Materials
+    /**
+     * Sizes
+     */
+    sizes = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+    // Objects
+    const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+    // Materials
 
-// Lights
+    const material = new THREE.MeshBasicMaterial()
+    material.color = new THREE.Color(0xff0000)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+    // Mesh
+    sphere = new THREE.Mesh(geometry,material)
+    scene.add(sphere)
+    
+    // Lights
 
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+    const pointLight = new THREE.PointLight(0xffffff, 0.1)
+    pointLight.position.x = 2
+    pointLight.position.y = 3
+    pointLight.position.z = 4
+    scene.add(pointLight)
+
+
+    /**
+     * Camera
+     */
+    // Base camera
+    camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+    camera.position.x = 0
+    camera.position.y = 0
+    camera.position.z = 2
+    scene.add(camera)
+
+    // Controls
+    // const controls = new OrbitControls(camera, canvas)
+    // controls.enableDamping = true
+
+    /**
+     * Renderer
+     */
+    renderer = new THREE.WebGLRenderer({
+        canvas: canvas
+    })
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    window.addEventListener('resize', windowResizeHandler)
 }
 
-window.addEventListener('resize', () =>
-{
+const windowResizeHandler = () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -53,30 +83,7 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
-
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+}
 
 /**
  * Animate
@@ -84,22 +91,28 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 const clock = new THREE.Clock()
 
-const tick = () =>
+const animate = () =>
 {
+
+    // Update Orbital Controls
+    // controls.update()
+
+    render()
+
+    // Call animate again on the next frame
+    window.requestAnimationFrame(animate)
+}
+
+const render = () => {
 
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
     sphere.rotation.y = .5 * elapsedTime
 
-    // Update Orbital Controls
-    // controls.update()
-
     // Render
     renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
 }
 
-tick()
+init();
+animate();
