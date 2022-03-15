09470bce8a3b2618234f5c78b6d5b5c43dca8bdc
lenin mamani <lenin.mamani@tannua.com>
2022-03-15 17:25:01 -0500

    starter glb

--- src/script.js
+++ src/script.js
@@ -22,7 +22,7 @@ const init = () => {
     scene = new THREE.Scene()
     
     // Base camera
-    camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+    camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
     camera.position.set(0,5,25)
     scene.add(camera)
 
